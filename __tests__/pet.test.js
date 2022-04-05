b8ccdac75fb1d1437ed4f6da693c89e03bb8ff4d
Anna Chan <annachan200@gmail.com>
2022-04-05 15:08:14 +0100

    getting older

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,4 +4,22 @@ describe('constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
-  });.exit
+
+  });
+
+describe('constructor', () => {
+    it('has an initial age of 0', () => {
+       const pet = new Pet ('Fido');
+       expect(pet.age).toEqual(0); 
+    })
+  })
+
+describe('growUp'() => {
+    it('increments the age by 1', () => {
+        const pet = new Pet('Fido');
+    
+        pet.growUp();
+    
+        expect(pet.age).toEqual(1);
+      });
+    }); 
--- src/pet.js
+++ src/pet.js
@@ -1,6 +1,15 @@
-function Pet(name) {
+function Pet(name,) {
     this.name = name;
+    this.age = 0;
 }
-var fido = new Pet('Fido')
+Pet.prototype.growUp = function () {
+    this.age += 1;
+};
+
+const fido = new Pet ('Fido');
+const rex = new Pet ('Rex');
+const cleo = new Pet ('Cleo');
+
+}
 
 module.exports = Pet;
