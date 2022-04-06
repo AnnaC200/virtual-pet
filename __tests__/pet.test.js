3064b49b1251ddc8d21e2b2286b8da46ca904a21
Anna Chan <annachan200@gmail.com>
2022-04-06 21:02:42 +0100

    getting older, getting unhealthier

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -3,23 +3,36 @@ const Pet = require('./src/pet.js')
 describe('constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
-    });
-
-  });
-
-describe('constructor', () => {
+});
+    it('sets the name property', () => {
+      const pet = new Pet('Fido');
+      expect(pet.name).toEqual('Fido');
+});
     it('has an initial age of 0', () => {
        const pet = new Pet ('Fido');
        expect(pet.age).toEqual(0); 
-    })
-  })
+})
+    it('has an initial hunger of 0', () => {
+      expect(pet.hunger)toEqual(0);
+})
+    it('has an initial fitness of 0', () => {
+      expect(pet.fitness)toEqual(10);
+})
+})
 
-describe('growUp'() => {
+describe('growUp', () => {
     it('increments the age by 1', () => {
-        const pet = new Pet('Fido');
-    
-        pet.growUp();
-    
-        expect(pet.age).toEqual(1);
-      });
-    }); 
+      const pet = new Pet('Fido');
+      pet.growUp();
+      expect(pet.age).toEqual(1);
+  });
+    it('increase the hunger by 5', () => {
+      expect(pet.hunger).toEqual(5);
+  })
+    it('decrease the fitness by 3', () => {
+      expect(pet.fitness).toEqual(-3);
+    })
+
+});
+
+
--- src/pet.js
+++ src/pet.js
@@ -1,15 +1,19 @@
-function Pet(name,) {
+function Pet(name) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
 }
-Pet.prototype.growUp = function () {
+Pet.prototype.growUp = function() {
     this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
 };
 
-const fido = new Pet ('Fido');
-const rex = new Pet ('Rex');
-const cleo = new Pet ('Cleo');
+const fido = new Pet('Fido');
 
-}
+
+
+
 
 module.exports = Pet;
